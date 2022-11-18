1fe9bc3b97237af42f41e9ee57ed3ef6fa9b4c77
Timothé LARIVIERE <timothe.lariviere@outlook.com>
2022-11-18 16:25:12 +0100

    .

--- samples/ControlCatalog/ControlCatalog.Web/Program.fs
+++ samples/ControlCatalog/ControlCatalog.Web/Program.fs
@@ -4,12 +4,13 @@ open System.Runtime.Versioning
 open Avalonia
 open Avalonia.Web
 open ControlCatalog
+open Fabulous.Avalonia
 
-[<assembly:SupportedOSPlatform("browser")>]
-type Program() =
-    static member BuildAvaloniaApp() =
-        AppBuilder.Configure<App>()
-           
-    static member Main(args: string array) =
-        BuildAvaloniaApp()
+[<assembly: SupportedOSPlatform("browser")>]
+do ()
+
+module Program =
+    let main _args =
+        AppBuilder
+            .Configure(fun () -> Program.startApplication App.program)
             .SetupBrowserApp("out")
