837dae7b854c92f0fc507d1601095211d7f1a69a
Edgar <edgar.gonzalez@fundourselves.com>
2022-12-02 22:34:54 +0000

    Button + Modifiers

---
+++ src/Fabulous.Avalonia/Widgets/Controls/_FlyoutBase.fs
@@ -0,0 +1,61 @@
+namespace Fabulous.Avalonia
+
+open System.Runtime.CompilerServices
+open Avalonia.Controls
+open Avalonia.Controls.Primitives
+open Fabulous
+
+type IFabFlyoutBase =
+    inherit IFabElement
+
+module FlyoutBase =
+
+    let IsOpen = Attributes.defineAvaloniaPropertyWithEquality FlyoutBase.IsOpenProperty
+
+    let Target = Attributes.defineAvaloniaPropertyWidget FlyoutBase.TargetProperty
+
+    let Placement =
+        Attributes.defineAvaloniaPropertyWithEquality FlyoutBase.PlacementProperty
+
+    let ShowMode =
+        Attributes.defineAvaloniaPropertyWithEquality FlyoutBase.ShowModeProperty
+
+    let OverlayInputPassThrough =
+        Attributes.defineAvaloniaPropertyWidget FlyoutBase.OverlayInputPassThroughElementProperty
+
+    let AttachedFlyout =
+        Attributes.defineAvaloniaPropertyWidget FlyoutBase.AttachedFlyoutProperty
+
+[<Extension>]
+type FlyoutBaseModifiers =
+    [<Extension>]
+    static member inline isOpen(this: WidgetBuilder<'msg, #IFabFlyoutBase>, isOpen: bool) =
+        this.AddScalar(FlyoutBase.IsOpen.WithValue(isOpen))
+
+    [<Extension>]
+    static member inline target(this: WidgetBuilder<'msg, #IFabFlyoutBase>, target: WidgetBuilder<'msg, IFabControl>) =
+        this.AddWidget(FlyoutBase.Target.WithValue(target.Compile()))
+
+    [<Extension>]
+    static member inline placement(this: WidgetBuilder<'msg, #IFabFlyoutBase>, placement: FlyoutPlacementMode) =
+        this.AddScalar(FlyoutBase.Placement.WithValue(placement))
+
+    [<Extension>]
+    static member inline showMode(this: WidgetBuilder<'msg, #IFabFlyoutBase>, showMode: FlyoutShowMode) =
+        this.AddScalar(FlyoutBase.ShowMode.WithValue(showMode))
+
+    [<Extension>]
+    static member inline overlayInputPassThrough
+        (
+            this: WidgetBuilder<'msg, #IFabFlyoutBase>,
+            overlayInputPassThrough: WidgetBuilder<'msg, IFabInputElement>
+        ) =
+        this.AddWidget(FlyoutBase.OverlayInputPassThrough.WithValue(overlayInputPassThrough.Compile()))
+
+    [<Extension>]
+    static member inline attachedFlyout
+        (
+            this: WidgetBuilder<'msg, #IFabFlyoutBase>,
+            attachedFlyout: WidgetBuilder<'msg, IFabFlyoutBase>
+        ) =
+        this.AddWidget(FlyoutBase.AttachedFlyout.WithValue(attachedFlyout.Compile()))
