9b1124b8e2449d4fc3baef89f4ec0398e602d691
Edgar <edgar.gonzalez@fundourselves.com>
2022-12-03 20:40:14 +0000

    Panels

---
+++ src/Fabulous.Avalonia/Widgets/Controls/Panels/DockPanel.fs
@@ -0,0 +1,28 @@
+namespace Fabulous.Avalonia
+
+open Avalonia.Controls
+open Fabulous
+
+type IFabDockPanel =
+    inherit IFabPanel
+
+module DockPanel =
+    let WidgetKey = Widgets.register<DockPanel> ()
+
+    let Dock =
+        Attributes.defineAvaloniaPropertyWithEquality DockPanel.DockProperty
+        
+    let LastChildFill =
+        Attributes.defineAvaloniaPropertyWithEquality DockPanel.LastChildFillProperty
+
+[<AutoOpen>]
+module DockPanelBuilders =
+    type Fabulous.Avalonia.View with
+
+        static member Dock<'msg>(dock: Dock, ?lastChildFill: bool) =
+            CollectionBuilder<'msg, IFabDockPanel, IFabControl>(
+                DockPanel.WidgetKey,
+                Panel.Children,
+                DockPanel.Dock.WithValue(dock),
+                DockPanel.LastChildFill.WithValue(lastChildFill |> Option.defaultValue true)
+            )
