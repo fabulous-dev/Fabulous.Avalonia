875702771550ee0b61b2ac950d54157ac6991b74
Edgar <edgar.gonzalez@fundourselves.com>
2022-12-09 05:24:38 +0000

    Format code

--- src/Fabulous.Avalonia/Widgets/Controls/Panels/StackPanel.fs
+++ src/Fabulous.Avalonia/Widgets/Controls/Panels/StackPanel.fs
@@ -12,7 +12,7 @@ module StackPanel =
 
     let Spacing =
         Attributes.defineAvaloniaPropertyWithEquality StackPanel.SpacingProperty
-        
+
     let Orientation =
         Attributes.defineAvaloniaPropertyWithEquality StackPanel.OrientationProperty
 
