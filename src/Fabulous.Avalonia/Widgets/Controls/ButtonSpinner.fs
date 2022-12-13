b1be8194249872e8e4f77b66080bd1cfdd35af0e
ConradRoesch <conrad.roesch@live.com>
2022-12-13 13:33:03 -0800

    Add wrapper for Spinner (abstract base class) Add wrapper for ButtonSpinner

---
+++ src/Fabulous.Avalonia/Widgets/Controls/ButtonSpinner.fs
@@ -0,0 +1,51 @@
+﻿namespace Fabulous.Avalonia
+
+open Avalonia.Controls
+open Fabulous
+open Fabulous.StackAllocatedCollections
+open Fabulous.StackAllocatedCollections.StackList
+open System.Runtime.CompilerServices
+
+type IFabButtonSpinner =
+    inherit IFabSpinner
+
+module ButtonSpinner =
+    let WidgetKey = Widgets.register<ButtonSpinner> ()
+
+    let AllowSpin =
+        Attributes.defineAvaloniaPropertyWithEquality ButtonSpinner.AllowSpinProperty
+
+    let ButtonSpinnerLocation =
+        Attributes.defineAvaloniaPropertyWithEquality ButtonSpinner.ButtonSpinnerLocationProperty
+
+    let ShowButtonSpinner =
+        Attributes.defineAvaloniaPropertyWithEquality ButtonSpinner.ShowButtonSpinnerProperty
+
+[<AutoOpen>]
+module ButtonSpinnerBuilders =
+    type Fabulous.Avalonia.View with
+
+        static member inline ButtonSpinner(content: WidgetBuilder<'msg, #IFabControl>) =
+            WidgetBuilder<'msg, IFabButtonSpinner>(
+                ButtonSpinner.WidgetKey,
+                AttributesBundle(
+                    StackList.empty (),
+                    ValueSome [| ContentControl.Content.WithValue(content.Compile()) |],
+                    ValueNone
+                )
+            )
+
+[<Extension>]
+type ButtonSpinnerModifiers =
+
+    [<Extension>]
+    static member inline allowSpin(this: WidgetBuilder<'msg, #IFabButtonSpinner>, value: bool) =
+        this.AddScalar(ButtonSpinner.AllowSpin.WithValue(value))
+
+    [<Extension>]
+    static member inline buttonSpinnerLocation(this: WidgetBuilder<'msg, #IFabButtonSpinner>, value: Location) =
+        this.AddScalar(ButtonSpinner.ButtonSpinnerLocation.WithValue(value))
+
+    [<Extension>]
+    static member inline showButtonSpinner(this: WidgetBuilder<'msg, #IFabButtonSpinner>, value: bool) =
+        this.AddScalar(ButtonSpinner.ShowButtonSpinner.WithValue(value))
