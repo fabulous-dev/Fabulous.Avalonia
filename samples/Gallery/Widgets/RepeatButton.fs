4200827bff602cf1d5831409668a6d0c315d7cb7
ConradRoesch <conrad.roesch@live.com>
2022-12-24 02:39:02 -0800

    RepeatButton sample

---
+++ samples/Gallery/Widgets/RepeatButton.fs
@@ -0,0 +1,33 @@
+namespace Gallery
+
+open Avalonia.Media
+open Fabulous.Avalonia
+
+open type Fabulous.Avalonia.View
+
+module RepeatButton =
+    type Model = { Count: int }
+
+    type Msg = | Increment
+
+    let init () = { Count = 0 }
+
+    let update msg model =
+        match msg with
+        | Increment -> { model with Count = model.Count + 1 }
+
+    let view model =
+        VStack(spacing = 15.) {
+            TextBlock("Repeat button")
+
+            RepeatButton("Click me, or press and hold!", Increment)
+                .delay(400)
+                .interval (200)
+
+            TextBlock($"Count: {model.Count}").centerVertical ()
+        }
+
+    let sample =
+        { Name = "RepeatButton"
+          Description = "A button widget that reacts to single touch or repeats while depressed."
+          Program = Helper.createProgram init update view }
