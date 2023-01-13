2f06f3cbda64ab8b426f16f75f6e0ae097d049d7
edgardev <edgar.gonzalez@fundourselves.com>
2023-01-13 21:41:59 +0000

    format code

--- samples/Gallery/Widgets/RepeatButton.fs
+++ samples/Gallery/Widgets/RepeatButton.fs
@@ -17,18 +17,20 @@ module RepeatButton =
 
     let view _ =
         VStack(spacing = 15.) {
-            RepeatButton("Click me, or press and hold!", Clicked)
+            RepeatButton("Click me, or press and hold!", Clicked).delay(400).interval (200)
+
+            RepeatButton(
+                Clicked,
+                HStack(16.) {
+                    Image(ImageSource.fromString "avares://Gallery/Assets/Icons/fabulous-icon.png")
+                        .width(20.)
+                        .height (20.)
+
+                    TextBlock("Example with custom content")
+                }
+            )
                 .delay(400)
-                .interval(200)
-                
-            RepeatButton(Clicked, HStack(16.){
-                Image(ImageSource.fromString "avares://Gallery/Assets/Icons/fabulous-icon.png")
-                    .width(20.)
-                    .height(20.)
-                TextBlock("Example with custom content")
-            })
-                .delay(400)
-                .interval(200)
+                .interval (200)
         }
 
     let sample =
