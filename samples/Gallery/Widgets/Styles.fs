9e2b6c8b97e7b71a7146460ef2023a9d6e22699f
edgardev <edgar.gonzalez@fundourselves.com>
2023-02-08 21:16:45 +0000

    More samples for Styles

--- samples/Gallery/Widgets/Styles.fs
+++ samples/Gallery/Widgets/Styles.fs
@@ -23,6 +23,23 @@ module Styles =
             TextBlock("I'm a Heading1!").classes([ "h1" ])
 
             TextBlock("I'm a Heading2!").classes([ "h2" ])
+
+            TextBlock("I'm a Heading3!").classes([ "h3" ])
+
+            TextBlock("I'm a Heading4!").classes([ "h4" ])
+
+            TextBlock("I'm a Heading5!").classes([ "h5" ])
+
+            TextBlock("I'm a Heading6!").classes([ "h6" ])
+
+            TextBlock("I'm a Body1!").classes([ "h7" ])
+
+            TextBlock("I'm a Body2!").classes([ "h8" ])
+
+            TextBlock("I'm a Body3!").classes([ "h9" ])
+
+            TextBlock("I'm just a text")
+
         })
 
     let sample =
