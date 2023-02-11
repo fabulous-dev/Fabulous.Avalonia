94d554ca253af96dfc54213b554046bc3fd0b90f
edgardev <edgar.gonzalez@fundourselves.com>
2023-02-11 19:42:21 +0000

    more modifiers

--- src/Fabulous.Avalonia/Widgets/Controls/_ItemsControl.fs
+++ src/Fabulous.Avalonia/Widgets/Controls/_ItemsControl.fs
@@ -21,6 +21,12 @@ module ItemsControl =
     let AreVerticalSnapPointsRegular =
         Attributes.defineAvaloniaPropertyWithEquality ItemsControl.AreVerticalSnapPointsRegularProperty
 
+    let HorizontalSnapPointsChanged =
+        Attributes.defineEvent "ItemsControl_HorizontalSnapPointsChanged" (fun target -> (target :?> ItemsControl).HorizontalSnapPointsChanged)
+
+    let VerticalSnapPointsChanged =
+        Attributes.defineEvent "ItemsControl_VerticalSnapPointsChanged" (fun target -> (target :?> ItemsControl).VerticalSnapPointsChanged)
+
 [<Extension>]
 type ItemsControlModifiers =
     [<Extension>]
@@ -34,3 +40,11 @@ type ItemsControlModifiers =
     [<Extension>]
     static member inline areVerticalSnapPointsRegular(this: WidgetBuilder<'msg, #IFabItemsControl>, value: bool) =
         this.AddScalar(ItemsControl.AreVerticalSnapPointsRegular.WithValue(value))
+
+    [<Extension>]
+    static member inline onHorizontalSnapPointsChanged(this: WidgetBuilder<'msg, #IFabItemsControl>, onHorizontalSnapPointsChanged: 'msg) =
+        this.AddScalar(ItemsControl.HorizontalSnapPointsChanged.WithValue(fun _ -> onHorizontalSnapPointsChanged |> box))
+
+    [<Extension>]
+    static member inline onVerticalSnapPointsChanged(this: WidgetBuilder<'msg, #IFabItemsControl>, onVerticalSnapPointsChanged: 'msg) =
+        this.AddScalar(ItemsControl.VerticalSnapPointsChanged.WithValue(fun _ -> onVerticalSnapPointsChanged |> box))
