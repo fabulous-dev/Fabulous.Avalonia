c8a5bf886e6bd9f4f2659fdcdee374681e795070
edgardev <edgar.gonzalez@fundourselves.com>
2023-03-10 23:36:53 +0100

    Porting GameOfLife

---
+++ samples/GameOfLife/Platform/Android/MainActivity.fs
@@ -0,0 +1,13 @@
+namespace GameOfLife
+
+open Android.App
+open Android.Content.PM
+open Avalonia.Android
+
+[<Activity(Label = "GameOfLife.Android",
+           Theme = "@style/MyTheme.NoActionBar",
+           Icon = "@drawable/icon",
+           LaunchMode = LaunchMode.SingleTop,
+           ConfigurationChanges = (ConfigChanges.Orientation ||| ConfigChanges.ScreenSize))>]
+type MainActivity() =
+    inherit AvaloniaMainActivity()
