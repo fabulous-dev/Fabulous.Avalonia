6482f6dcc97a9c05d6e7f60e1c12b379d83f2c82
edgardev <edgar.gonzalez@fundourselves.com>
2023-03-19 10:14:33 +0100

    SnakeGame Browser

---
+++ samples/SnakeGame/Platform/Browser/Program.fs
@@ -0,0 +1,20 @@
+namespace SnakeGame.Browser
+
+open System.Runtime.Versioning
+open Avalonia
+open Avalonia.Browser
+open Fabulous.Avalonia
+open SnakeGame
+
+module public Program =
+    [<assembly: SupportedOSPlatform("browser")>]
+    do ()
+
+    [<CompiledName "BuildAvaloniaApp">]
+    let public buildAvaloniaApp () =
+        AppBuilder.Configure(fun () -> Program.startApplication App.program)
+
+    [<EntryPoint>]
+    let main argv =
+        buildAvaloniaApp().SetupBrowserApp("out") |> ignore
+        0
