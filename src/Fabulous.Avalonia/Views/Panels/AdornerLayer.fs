f1fbbce9e091c3e937c02f9ea492fcd89406a2cd
edgardev <edgar.gonzalez@fundourselves.com>
2023-04-13 22:29:14 +0100

    Fix AdornerLayer + sample

--- src/Fabulous.Avalonia/Views/Panels/AdornerLayer.fs
+++ src/Fabulous.Avalonia/Views/Panels/AdornerLayer.fs
@@ -3,7 +3,6 @@ namespace Fabulous.Avalonia
 open System.Runtime.CompilerServices
 open Avalonia.Controls.Primitives
 open Fabulous
-open Fabulous.StackAllocatedCollections.StackList
 
 type IFabAdornerLayer =
     inherit IFabCanvas
@@ -13,18 +12,12 @@ module AdornerLayer =
 
     let Adorner = Attributes.defineAvaloniaPropertyWidget AdornerLayer.AdornerProperty
 
-[<AutoOpen>]
-module AdornerLayerBuilders =
-    type Fabulous.Avalonia.View with
-
-        static member inline AdornerLayer(content: WidgetBuilder<'msg, #IFabControl>) =
-            WidgetBuilder<'msg, IFabAdornerLayer>(
-                AdornerLayer.WidgetKey,
-                AttributesBundle(StackList.empty(), ValueSome [| AdornerLayer.Adorner.WithValue(content.Compile()) |], ValueNone)
-            )
-
 [<Extension>]
 type AdornerLayerModifiers =
+    [<Extension>]
+    static member inline adorner(this: WidgetBuilder<'msg, #IFabVisual>, widget: WidgetBuilder<'msg, #IFabControl>) =
+        this.AddWidget(AdornerLayer.Adorner.WithValue(widget.Compile()))
+
     /// <summary>Link a ViewRef to access the direct AdornerLayer control instance</summary>
     /// <param name="this">Current widget</param>
     /// <param name="value">The ViewRef instance that will receive access to the underlying control</param>
