27b929df9d22afc1e19135e420150af7645b2059
edgardev <edgar.gonzalez@fundourselves.com>
2023-04-13 18:38:08 +0100

    more docs

--- src/Fabulous.Avalonia/Views/_Control.fs
+++ src/Fabulous.Avalonia/Views/_Control.fs
@@ -36,38 +36,78 @@ module Control =
 
 [<Extension>]
 type ControlModifiers =
+    /// <summary>Set the ContextFlyout widget</summary>
+    /// <param name="this">Current widget</param>
+    /// <param name="widget">The widget to set</param>
+    /// <example>
+    /// <code>
+    /// Border(TextBlock("Flyout that can be applied to any control."))
+    ///    .contextFlyout(
+    ///        MenuFlyout() {
+    ///            MenuItem("Menu Item")
+    ///            ...
+    ///        }
+    ///    )
+    /// </code>
+    /// </example>
     [<Extension>]
-    static member inline contextFlyout(this: WidgetBuilder<'msg, #IFabControl>, content: WidgetBuilder<'msg, #IFabFlyoutBase>) =
-        this.AddWidget(Control.ContextFlyout.WithValue(content.Compile()))
+    static member inline contextFlyout(this: WidgetBuilder<'msg, #IFabControl>, widget: WidgetBuilder<'msg, #IFabFlyoutBase>) =
+        this.AddWidget(Control.ContextFlyout.WithValue(widget.Compile()))
 
+    /// <summary>Set the Tag property</summary>
+    /// <param name="this">Current widget</param>
+    /// <param name="value">The value to set</param>
     [<Extension>]
     static member inline tag(this: WidgetBuilder<'msg, #IFabControl>, value: string) =
         this.AddScalar(Control.Tag.WithValue(value))
 
+    /// <summary>Set the FlowDirection property</summary>
+    /// <param name="this">Current widget</param>
+    /// <param name="value">The value to set</param>
+    /// <example>
+    /// <code>
+    /// [&lt;Struct&gt;]
+    /// type FlowDirection =
+    /// | LeftToRight = 0
+    /// | RightToLeft = 1
+    /// </code>
+    /// </example>
     [<Extension>]
     static member inline flowDirection(this: WidgetBuilder<'msg, #IFabControl>, value: FlowDirection) =
         this.AddScalar(Control.FlowDirection.WithValue(value))
 
+    /// <summary>Listens to the ContextRequested event</summary>
+    /// <param name="this">Current widget</param>
+    /// <param name="fn">Function to be call when the event is raised</param>
     [<Extension>]
-    static member inline onContextRequested(this: WidgetBuilder<'msg, #IFabControl>, onContextRequested: ContextRequestedEventArgs -> 'msg) =
-        this.AddScalar(Control.ContextRequested.WithValue(fun args -> onContextRequested args |> box))
+    static member inline onContextRequested(this: WidgetBuilder<'msg, #IFabControl>, fn: ContextRequestedEventArgs -> 'msg) =
+        this.AddScalar(Control.ContextRequested.WithValue(fun args -> fn args |> box))
 
+    /// <summary>Listens to the Loaded event</summary>
+    /// <param name="this">Current widget</param>
+    /// <param name="fn">Function to be call when the event is raised</param>
     [<Extension>]
-    static member inline onLoaded(this: WidgetBuilder<'msg, #IFabControl>, onLoaded: bool -> 'msg) =
+    static member inline onLoaded(this: WidgetBuilder<'msg, #IFabControl>, fn: bool -> 'msg) =
         this.AddScalar(
             Control.Loaded.WithValue(fun args ->
                 let control = args.Source :?> Control
-                onLoaded control.IsLoaded |> box)
+                fn control.IsLoaded |> box)
         )
 
+    /// <summary>Listens to the UnLoaded event</summary>
+    /// <param name="this">Current widget</param>
+    /// <param name="fn">Function to be call when the event is raised</param>
     [<Extension>]
-    static member inline onUnLoaded(this: WidgetBuilder<'msg, #IFabControl>, onUnLoaded: bool -> 'msg) =
+    static member inline onUnLoaded(this: WidgetBuilder<'msg, #IFabControl>, fn: bool -> 'msg) =
         this.AddScalar(
             Control.UnLoaded.WithValue(fun args ->
                 let control = args.Source :?> Control
-                onUnLoaded control.IsLoaded |> box)
+                fn control.IsLoaded |> box)
         )
 
+    /// <summary>Listens to the SizeChanged event</summary>
+    /// <param name="this">Current widget</param>
+    /// <param name="fn">Function to be call when the event is raised</param>
     [<Extension>]
-    static member inline onSizeChanged(this: WidgetBuilder<'msg, #IFabControl>, onSizeChanged: SizeChangedEventArgs -> 'msg) =
-        this.AddScalar(Control.SizeChanged.WithValue(fun args -> onSizeChanged args |> box))
+    static member inline onSizeChanged(this: WidgetBuilder<'msg, #IFabControl>, fn: SizeChangedEventArgs -> 'msg) =
+        this.AddScalar(Control.SizeChanged.WithValue(fun args -> fn args |> box))
