a6a6ae704540666d17cfa006fa817520e4fa61a8
edgardev <edgar.gonzalez@fundourselves.com>
2023-04-29 16:32:43 +0100

    CustomAnimator sample

--- samples/Gallery/Pages/Animations/AnimationsPage.fs
+++ samples/Gallery/Pages/Animations/AnimationsPage.fs
@@ -9,15 +9,18 @@ open type Fabulous.Avalonia.View
 module AnimationsPage =
     type Model =
         { Animations1: Animations1.Model
-          Animations2: Animations2.Model }
+          Animations2: Animations2.Model
+          Animations3: Animations3.Model }
 
     type Msg =
         | Animations1 of Animations1.Msg
         | Animations2 of Animations2.Msg
+        | Animations3 of Animations3.Msg
 
     let init () =
         { Animations1 = Animations1.init()
-          Animations2 = Animations2.init() }
+          Animations2 = Animations2.init()
+          Animations3 = Animations3.init() }
 
     let update msg model =
         match msg with
@@ -33,10 +36,17 @@ module AnimationsPage =
             { model with
                 Animations2 = transitions2 }
 
+        | Animations3 msg ->
+            let transitions3 = Animations3.update msg model.Animations3
+
+            { model with
+                Animations3 = transitions3 }
+
 
     let view model =
         (VStack(32.) {
             View.map Animations1 (Animations1.view model)
             View.map Animations2 (Animations2.view model)
+            View.map Animations3 (Animations3.view model)
         })
             .clock(Clock())
