ef2fe93075720695a919e3f3b207712fb5ef0b7e
edgardev <edgar.gonzalez@fundourselves.com>
2023-06-01 17:49:57 +0100

    .

--- samples/Gallery/Pages/ComboBoxPage.fs
+++ samples/Gallery/Pages/ComboBoxPage.fs
@@ -9,15 +9,9 @@ open Gallery
 
 module ComboBoxPage =
 
-    type Figure =
-        | Rectangles
-        | Ellipses
-        | Unknown
-
     type Model =
         { Items: string list
           Fonts: FontFamily seq
-          Figures: Figure list
           IsDropDownOpen: bool }
 
     type Msg = DropDownOpened of bool
@@ -34,7 +28,6 @@ module ComboBoxPage =
     let init () =
         { IsDropDownOpen = false
           Items = [ "Inline Items"; "Inline Item 2"; "Inline Item 3"; "Inline Item 4" ]
-          Figures = [ Rectangles; Ellipses; Unknown ]
           Fonts = fontComboBox() },
         []
 
@@ -46,14 +39,11 @@ module ComboBoxPage =
         HStack(16) {
             ComboBox(model.Items, (fun x -> TextBlock(x))).selectedIndex(0)
 
-            (ComboBox(
-                model.Figures,
-                fun x ->
-                    match x with
-                    | Rectangles -> VStack() { Rectangle().height(10.).fill(SolidColorBrush(Colors.Red)) }
-                    | Ellipses -> VStack() { Ellipse().size(50., 50.).fill(SolidColorBrush(Colors.Yellow)) }
-                    | Unknown -> VStack() { TextBlock("Unknown") }
-            ))
+            (ComboBox() {
+                ComboBoxItem(Rectangle().size(10., 50.).fill(SolidColorBrush(Colors.Red)))
+                ComboBoxItem(Ellipse().size(50., 50.).fill(SolidColorBrush(Colors.Yellow)))
+                ComboBoxItem(TextBlock("Unknown"))
+            })
                 .selectedIndex(0)
 
             (ComboBox() {
