320ef970298beab004a521d23b4a4c66829efcb2
edgardev <edgar.gonzalez@fundourselves.com>
2023-07-01 16:33:01 +0200

    Update itemsControls

--- src/Fabulous.Avalonia/Views/_ItemsControl.fs
+++ src/Fabulous.Avalonia/Views/_ItemsControl.fs
@@ -40,18 +40,6 @@ module ItemsControl =
                     listBox.SetValue(ItemsControl.ItemsSourceProperty, value.OriginalItems)
                     |> ignore)
 
-    let AreHorizontalSnapPointsRegular =
-        Attributes.defineAvaloniaPropertyWithEquality ItemsControl.AreHorizontalSnapPointsRegularProperty
-
-    let AreVerticalSnapPointsRegular =
-        Attributes.defineAvaloniaPropertyWithEquality ItemsControl.AreVerticalSnapPointsRegularProperty
-
-    let HorizontalSnapPointsChanged =
-        Attributes.defineEvent "ItemsControl_HorizontalSnapPointsChanged" (fun target -> (target :?> ItemsControl).HorizontalSnapPointsChanged)
-
-    let VerticalSnapPointsChanged =
-        Attributes.defineEvent "ItemsControl_VerticalSnapPointsChanged" (fun target -> (target :?> ItemsControl).VerticalSnapPointsChanged)
-
     let ContainerClearing =
         Attributes.defineEvent "ItemsControl_ContainerClearing" (fun target -> (target :?> ItemsControl).ContainerClearing)
 
@@ -74,22 +62,6 @@ module ItemsControlBuilders =
 
 [<Extension>]
 type ItemsControlModifiers =
-    [<Extension>]
-    static member inline areHorizontalSnapPointsRegular(this: WidgetBuilder<'msg, #IFabItemsControl>, value: bool) =
-        this.AddScalar(ItemsControl.AreHorizontalSnapPointsRegular.WithValue(value))
-
-    [<Extension>]
-    static member inline areVerticalSnapPointsRegular(this: WidgetBuilder<'msg, #IFabItemsControl>, value: bool) =
-        this.AddScalar(ItemsControl.AreVerticalSnapPointsRegular.WithValue(value))
-
-    [<Extension>]
-    static member inline onHorizontalSnapPointsChanged(this: WidgetBuilder<'msg, #IFabItemsControl>, onHorizontalSnapPointsChanged: 'msg) =
-        this.AddScalar(ItemsControl.HorizontalSnapPointsChanged.WithValue(fun _ -> onHorizontalSnapPointsChanged |> box))
-
-    [<Extension>]
-    static member inline onVerticalSnapPointsChanged(this: WidgetBuilder<'msg, #IFabItemsControl>, onVerticalSnapPointsChanged: 'msg) =
-        this.AddScalar(ItemsControl.VerticalSnapPointsChanged.WithValue(fun _ -> onVerticalSnapPointsChanged |> box))
-
     [<Extension>]
     static member inline onContainerClearing(this: WidgetBuilder<'msg, #IFabItemsControl>, onContainerClearing: ContainerClearingEventArgs -> 'msg) =
         this.AddScalar(ItemsControl.ContainerClearing.WithValue(fun args -> onContainerClearing args |> box))
