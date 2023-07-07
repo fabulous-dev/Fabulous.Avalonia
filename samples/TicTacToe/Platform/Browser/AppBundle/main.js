3de6c09ea48671d4e4523ee033f50680b15abbaa
edgardev <edgar.gonzalez@fundourselves.com>
2023-07-07 13:22:08 +0200

    Browser Samples are working again

--- samples/TicTacToe/Platform/Browser/AppBundle/main.js
+++ samples/TicTacToe/Platform/Browser/AppBundle/main.js
@@ -1,5 +1,7 @@
-﻿import { dotnet } from './dotnet.js'
-import { registerAvaloniaModule } from './avalonia.js';
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+import { dotnet } from './dotnet.js'
 
 const is_browser = typeof window != "undefined";
 if (!is_browser) throw new Error(`Expected to be running in a browser`);
@@ -9,8 +11,6 @@ const dotnetRuntime = await dotnet
     .withApplicationArgumentsFromQuery()
     .create();
 
-await registerAvaloniaModule(dotnetRuntime);
-
 const config = dotnetRuntime.getConfig();
 
-await dotnetRuntime.runMainAndExit(config.mainAssemblyName, [window.location.search]);
+await dotnetRuntime.runMainAndExit(config.mainAssemblyName, ["dotnet", "is", "great!"]);
