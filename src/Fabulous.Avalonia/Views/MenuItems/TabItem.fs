b78f23b94b068898f67b4b556c543c76bb83f648
edgardev <edgar.gonzalez@fundourselves.com>
2023-07-16 20:13:33 +0200

    MenuItems XML docs

--- src/Fabulous.Avalonia/Views/MenuItems/TabItem.fs
+++ src/Fabulous.Avalonia/Views/MenuItems/TabItem.fs
@@ -21,6 +21,9 @@ module TabItem =
 module TabItemBuilders =
     type Fabulous.Avalonia.View with
 
+        /// <summary>Creates a TabItem widget</summary>
+        /// <param name="header">The header of the TabItem</param>
+        /// <param name="content">The content of the TabItem</param>
         static member TabItem(header: string, content: WidgetBuilder<'msg, #IFabControl>) =
             WidgetBuilder<'msg, IFabTabItem>(
                 TabItem.WidgetKey,
@@ -31,6 +34,9 @@ module TabItemBuilders =
                 )
             )
 
+        /// <summary>Creates a TabItem widget</summary>
+        /// <param name="header">The header of the TabItem</param>
+        /// <param name="content">The content of the TabItem</param>
         static member TabItem(header: WidgetBuilder<'msg, #IFabControl>, content: WidgetBuilder<'msg, #IFabControl>) =
             WidgetBuilder<'msg, IFabTabItem>(
                 TabItem.WidgetKey,
@@ -45,10 +51,16 @@ module TabItemBuilders =
 
 [<Extension>]
 type TabItemModifiers =
+    /// <summary>Sets the TabStripPlacement property.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="value">The TabStripPlacement value</param>
     [<Extension>]
     static member inline tabStripPlacement(this: WidgetBuilder<'msg, #IFabTabItem>, value: Dock) =
         this.AddScalar(TabItem.TabStripPlacement.WithValue(value))
 
+    /// <summary>Sets the IsSelected property.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="value">The IsSelected value</param>
     [<Extension>]
     static member inline isSelected(this: WidgetBuilder<'msg, #IFabTabItem>, value: bool) =
         this.AddScalar(TabItem.IsSelected.WithValue(value))
