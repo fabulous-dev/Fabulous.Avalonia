eaebc30b1b00534153feea05c610d285430d7080
edgardev <edgar.gonzalez@fundourselves.com>
2023-07-17 19:44:21 +0200

    Layouts

--- src/Fabulous.Avalonia/Views/Layouts/SliptView.fs
+++ src/Fabulous.Avalonia/Views/Layouts/SliptView.fs
@@ -67,6 +67,9 @@ module SplitView =
 module SplitViewBuilders =
     type Fabulous.Avalonia.View with
 
+        /// <summary>Creates a SplitView widget</summary>
+        /// <param name="pane">The content of the pane</param>
+        /// <param name="content">The content to display</param>
         static member inline SplitView(pane: WidgetBuilder<'msg, #IFabControl>, content: WidgetBuilder<'msg, #IFabControl>) =
             WidgetBuilder<'msg, IFabSplitView>(
                 SplitView.WidgetKey,
@@ -81,71 +84,118 @@ module SplitViewBuilders =
 
 [<Extension>]
 type SplitViewModifiers =
-
+    /// <summary>Sets the CompactPaneLength property.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="value">The CompactPaneLength value</param>
     [<Extension>]
     static member inline compactPaneLength(this: WidgetBuilder<'msg, #IFabSplitView>, value: float) =
         this.AddScalar(SplitView.CompactPaneLength.WithValue(value))
 
+    /// <summary>Sets the DisplayMode property.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="value">The DisplayMode value</param>
     [<Extension>]
     static member inline displayMode(this: WidgetBuilder<'msg, #IFabSplitView>, value: SplitViewDisplayMode) =
         this.AddScalar(SplitView.DisplayMode.WithValue(value))
 
+    /// <summary>Sets the IsPaneOpen property.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="value">The IsPaneOpen value</param>
     [<Extension>]
     static member inline isPaneOpen(this: WidgetBuilder<'msg, #IFabSplitView>, value: bool) =
         this.AddScalar(SplitView.IsPaneOpen.WithValue(value))
 
+    /// <summary>Sets the OpenPaneLength property.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="value">The OpenPaneLength value</param>
     [<Extension>]
     static member inline openPaneLength(this: WidgetBuilder<'msg, #IFabSplitView>, value: float) =
         this.AddScalar(SplitView.OpenPaneLength.WithValue(value))
 
+    /// <summary>Sets the PaneBackground property.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="value">The PaneBackground value</param>
     [<Extension>]
-    static member inline paneBackground(this: WidgetBuilder<'msg, #IFabSplitView>, content: WidgetBuilder<'msg, #IFabBrush>) =
-        this.AddWidget(SplitView.PaneBackgroundWidget.WithValue(content.Compile()))
+    static member inline paneBackground(this: WidgetBuilder<'msg, #IFabSplitView>, value: WidgetBuilder<'msg, #IFabBrush>) =
+        this.AddWidget(SplitView.PaneBackgroundWidget.WithValue(value.Compile()))
 
+    /// <summary>Sets the PaneBackground property.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="value">The PaneBackground value</param>
     [<Extension>]
-    static member inline paneBackground(this: WidgetBuilder<'msg, #IFabSplitView>, brush: IBrush) =
-        this.AddScalar(SplitView.PaneBackground.WithValue(brush))
+    static member inline paneBackground(this: WidgetBuilder<'msg, #IFabSplitView>, value: IBrush) =
+        this.AddScalar(SplitView.PaneBackground.WithValue(value))
 
+    /// <summary>Sets the PaneBackground property.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="value">The PaneBackground value</param>
     [<Extension>]
-    static member inline paneBackground(this: WidgetBuilder<'msg, #IFabSplitView>, brush: string) =
-        this.AddScalar(SplitView.PaneBackground.WithValue(brush |> Color.Parse |> ImmutableSolidColorBrush))
+    static member inline paneBackground(this: WidgetBuilder<'msg, #IFabSplitView>, value: string) =
+        this.AddScalar(SplitView.PaneBackground.WithValue(value |> Color.Parse |> ImmutableSolidColorBrush))
 
+    /// <summary>Sets the PanePlacement property.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="value">The PanePlacement value</param>
     [<Extension>]
     static member inline panePlacement(this: WidgetBuilder<'msg, #IFabSplitView>, value: SplitViewPanePlacement) =
         this.AddScalar(SplitView.PanePlacement.WithValue(value))
 
+    /// <summary>Sets the UseLightDismissOverlayMode property.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="value">The UseLightDismissOverlayMode value</param>
     [<Extension>]
     static member inline useLightDismissOverlayMode(this: WidgetBuilder<'msg, #IFabSplitView>, value: bool) =
         this.AddScalar(SplitView.UseLightDismissOverlayMode.WithValue(value))
 
+    /// <summary>Sets the ClosedPaneWidth event.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="value">The ClosedPaneWidth value</param>
     [<Extension>]
     static member inline closedPaneWidth(this: WidgetBuilder<'msg, #IFabSplitView>, value: float) =
         this.AddScalar(SplitView.ClosedPaneWidth.WithValue(value))
 
+    /// <summary>Sets the PaneColumnGridLength event.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="value">The PaneColumnGridLength value</param>
     [<Extension>]
     static member inline paneColumnGridLength(this: WidgetBuilder<'msg, #IFabSplitView>, value: GridLength) =
         this.AddScalar(SplitView.PaneColumnGridLength.WithValue(value))
 
+    /// <summary>Listens to the SplitView PanClosed event.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="fn">Raised when the PanClosed event fires.</param>
     [<Extension>]
-    static member inline onPanClosed(this: WidgetBuilder<'msg, #IFabSplitView>, onPanClosed: RoutedEventArgs -> 'msg) =
-        this.AddScalar(SplitView.PanClosed.WithValue(fun args -> onPanClosed args |> box))
+    static member inline onPanClosed(this: WidgetBuilder<'msg, #IFabSplitView>, fn: RoutedEventArgs -> 'msg) =
+        this.AddScalar(SplitView.PanClosed.WithValue(fun args -> fn args |> box))
 
+    /// <summary>Listens to the SplitView PanClosing event.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="fn">Raised when the PanClosing event fires.</param>
     [<Extension>]
+    static member inline onPanClosing(this: WidgetBuilder<'msg, #IFabSplitView>, fn: CancelRoutedEventArgs -> 'msg) =
+        this.AddScalar(SplitView.PanClosing.WithValue(fun args -> fn args |> box))
 
-    static member inline onPanClosing(this: WidgetBuilder<'msg, #IFabSplitView>, onPanClosing: CancelRoutedEventArgs -> 'msg) =
-        this.AddScalar(SplitView.PanClosing.WithValue(fun args -> onPanClosing args |> box))
-
+    /// <summary>Listens to the SplitView PanOpened event.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="fn">Raised when the PanOpened event fires.</param>
     [<Extension>]
-    static member inline onPanOpened(this: WidgetBuilder<'msg, #IFabSplitView>, onPanOpened: RoutedEventArgs -> 'msg) =
-        this.AddScalar(SplitView.PanOpened.WithValue(fun args -> onPanOpened args |> box))
+    static member inline onPanOpened(this: WidgetBuilder<'msg, #IFabSplitView>, fn: RoutedEventArgs -> 'msg) =
+        this.AddScalar(SplitView.PanOpened.WithValue(fun args -> fn args |> box))
 
+    /// <summary>Listens to the SplitView PanOpening event.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="fn">Raised when the PanOpening event fires.</param>
     [<Extension>]
-    static member inline onPanOpening(this: WidgetBuilder<'msg, #IFabSplitView>, onPanOpening: CancelRoutedEventArgs -> 'msg) =
-        this.AddScalar(SplitView.PanOpening.WithValue(fun args -> onPanOpening args |> box))
+    static member inline onPanOpening(this: WidgetBuilder<'msg, #IFabSplitView>, fn: CancelRoutedEventArgs -> 'msg) =
+        this.AddScalar(SplitView.PanOpening.WithValue(fun args -> fn args |> box))
 
+    /// <summary>Listens to the SplitView IsPresented event.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="value">The IsPresented value</param>
+    /// <param name="fn">Raised when the IsPresented event fires.</param>
     [<Extension>]
-    static member inline isPresented(this: WidgetBuilder<'msg, #IFabSplitView>, value: bool, onChanged: bool -> 'msg) =
-        this.AddScalar(SplitView.IsPresented.WithValue(ValueEventData.create value (fun v -> onChanged v |> box)))
+    static member inline isPresented(this: WidgetBuilder<'msg, #IFabSplitView>, value: bool, fn: bool -> 'msg) =
+        this.AddScalar(SplitView.IsPresented.WithValue(ValueEventData.create value (fun v -> fn v |> box)))
 
     /// <summary>Link a ViewRef to access the direct SplitView control instance</summary>
     /// <param name="this">Current widget</param>
