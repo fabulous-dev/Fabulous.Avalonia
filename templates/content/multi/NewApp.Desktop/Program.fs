4f0ef763010027b045ee55ced804192a674b0291
Edgar Gonzalez <edgar.gonzalez@fundourselves.com>
2023-10-15 14:34:34 +0100

    .

--- templates/content/multi/NewApp.Desktop/Program.fs
+++ templates/content/multi/NewApp.Desktop/Program.fs
@@ -5,14 +5,15 @@ open Avalonia
 open Fabulous.Avalonia
 open NewApp
 
-// Initialization code. Don't use any Avalonia, third-party APIs or any
-// SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-// yet and stuff might break.
-[<STAThread; EntryPoint>]
-let Main (args: string array) =
+module Program =
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [<STAThread; EntryPoint>]
+    let Main (args: string array) =
 
-    AppBuilder
-        .Configure(fun () -> Program.startApplication App.program)
-        .UsePlatformDetect()
-        .LogToTrace(?level = None)
-        .StartWithClassicDesktopLifetime(args)
+        AppBuilder
+            .Configure(fun () -> Program.startApplication App.program)
+            .UsePlatformDetect()
+            .LogToTrace(?level = None)
+            .StartWithClassicDesktopLifetime(args)
