5d9437d0cfd6d597d9d5062701e9ffce7cf433fd
Edgar Gonzalez <edgar.gonzalez@fundourselves.com>
2023-10-15 18:47:15 +0100

    .

--- templates/content/multi/NewApp.Browser/AppBundle/main.js
+++ templates/content/multi/NewApp.Browser/AppBundle/main.js
@@ -1,4 +1,7 @@
-﻿import { dotnet } from './dotnet.js'
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+import { dotnet } from './dotnet.js'
 
 const is_browser = typeof window != "undefined";
 if (!is_browser) throw new Error(`Expected to be running in a browser`);
@@ -10,4 +13,4 @@ const dotnetRuntime = await dotnet
 
 const config = dotnetRuntime.getConfig();
 
-await dotnetRuntime.runMainAndExit(config.mainAssemblyName, [window.location.search]);
+await dotnetRuntime.runMainAndExit(config.mainAssemblyName, ["dotnet", "is", "great!"]);
