53d44bf9fd311cfc00830e06043088aaf513daa9
ConradRoesch <conrad.roesch@live.com>
2023-12-03 21:25:50 -0800

    Browser Template: Update path for JavaScript framework files (from .Net 8.0, Avalonia.Browser places these files in ./_framework)

--- templates/content/multi/NewApp.Browser/AppBundle/main.js
+++ templates/content/multi/NewApp.Browser/AppBundle/main.js
@@ -1,7 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-import { dotnet } from './dotnet.js'
+import { dotnet } from './_framework/dotnet.js'
 
 const is_browser = typeof window != "undefined";
 if (!is_browser) throw new Error(`Expected to be running in a browser`);
