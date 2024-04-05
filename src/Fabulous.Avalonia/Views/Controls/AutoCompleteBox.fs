f07bbce66627f4b264f5adc4ae5a4421e4401b3e
Holger Schmidt <leroyz.mailaccount@gmx.net>
2024-04-05 21:00:22 +0200

    fixed typo and added doco

--- src/Fabulous.Avalonia/Views/Controls/AutoCompleteBox.fs
+++ src/Fabulous.Avalonia/Views/Controls/AutoCompleteBox.fs
@@ -68,12 +68,12 @@ module AutoCompleteBox =
 module AutoCompleteBoxBuilders =
     type Fabulous.Avalonia.View with
 
-        /// <summary>Creates a AutoCompleteBox widget.</summary>
+        /// <summary>Creates an AutoCompleteBox widget.</summary>
         /// <param name="items">The items to display.</param>
         static member inline AutoCompleteBox(items: seq<_>) =
             WidgetBuilder<'msg, IFabAutoCompleteBox>(AutoCompleteBox.WidgetKey, AutoCompleteBox.ItemsSource.WithValue(items))
 
-        /// <summary>Creates a AutoCompleteBox widget.</summary>
+        /// <summary>Creates an AutoCompleteBox widget.</summary>
         /// <param name="populator">The function to populate the items.</param>
         static member inline AutoCompleteBox(populator: string -> CancellationToken -> Task<seq<_>>) =
             WidgetBuilder<'msg, IFabAutoCompleteBox>(AutoCompleteBox.WidgetKey, AutoCompleteBox.AsyncPopulator.WithValue(populator))
@@ -135,6 +135,10 @@ type AutoCompleteBoxModifiers =
     static member inline textFilter(this: WidgetBuilder<'msg, #IFabAutoCompleteBox>, value: string -> string -> bool) =
         this.AddScalar(AutoCompleteBox.TextFilter.WithValue(value))
 
+    /// <summary>Binds the AutoCompleteBox.TextProperty.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="text">The value to bind.</param>
+    /// <param name="fn">A function mapping the updated text to a 'msg to raise on user change.</param>
     [<Extension>]
     static member inline onTextChanged(this: WidgetBuilder<'msg, #IFabAutoCompleteBox>, text: string, fn: string -> 'msg) =
         this.AddScalar(AutoCompleteBox.TextChanged.WithValue(ValueEventData.create text fn))
