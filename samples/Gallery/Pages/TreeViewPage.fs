78262322d867d2f993352c98a3b234da591d4e9e
Holger Schmidt <leroyz.mailaccount@gmx.net>
2024-04-15 19:43:22 +0200

    added back the simple tree view and a top level page with tabs for different TreeViews

---
+++ samples/Gallery/Pages/TreeViewPage.fs
@@ -0,0 +1,12 @@
+namespace Gallery
+
+open Fabulous.Avalonia
+
+open type Fabulous.Avalonia.View
+
+module TreeViewPage =
+    let view () =
+        TabControl() {
+            TabItem("Simple", SimpleTreeView.view())
+            TabItem("With node interaction", TreeViewWithNodeInteraction.view())
+        }
