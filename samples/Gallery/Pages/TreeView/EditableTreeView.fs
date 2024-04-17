d547c0cc8b137ca777f213df1f887d45a7b5b1e4
Holger Schmidt <leroyz.mailaccount@gmx.net>
2024-04-17 23:10:29 +0200

    tried focusing added input

--- samples/Gallery/Pages/TreeView/EditableTreeView.fs
+++ samples/Gallery/Pages/TreeView/EditableTreeView.fs
@@ -1,12 +1,33 @@
 namespace Gallery
 
 open System.Diagnostics
+open System.Runtime.CompilerServices
 open Avalonia.Controls
+open Avalonia.Input
 open Avalonia.Layout
 open Fabulous.Avalonia
 open Fabulous
 
 open type Fabulous.Avalonia.View
+
+module FocusAttributes =
+    /// Allows setting the Focus on an Avalonia.Input.InputElement
+    let Focus =
+        Attributes.defineBool "Focus" (fun oldValueOpt newValueOpt node ->
+            let target = node.Target :?> InputElement
+
+            let rec focusAndCleanUp x y =
+                target.Focus() |> ignore
+                target.AttachedToVisualTree.RemoveHandler(focusAndCleanUp) // to clean up
+
+            if newValueOpt.IsSome && newValueOpt.Value then
+                target.AttachedToVisualTree.AddHandler(focusAndCleanUp))
+
+type FocusModifiers =
+    [<Extension>]
+    /// Sets the Focus on an IFabInputElement if set is true; otherwise does nothing.
+    static member inline focus(this: WidgetBuilder<'msg, #IFabInputElement>, set: bool) =
+        this.AddScalar(FocusAttributes.Focus.WithValue(set))
 
 module EditableNodeView =
     type Node(name, children) =
@@ -183,6 +204,7 @@ module EditableTreeView =
                             else
                                 EditableNodeView
                                     .view(node)
+                                    .focus(node.Name = "")
                                     .horizontalAlignment(HorizontalAlignment.Left)
 
                                 Button("x", RemoveNode node).tip(ToolTip("Remove"))
