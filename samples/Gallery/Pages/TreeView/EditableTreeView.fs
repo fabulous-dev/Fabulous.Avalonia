f9789ad1b09eb1fdc2266921ba65b2c81b537f41
Holger Schmidt <leroyz.mailaccount@gmx.net>
2024-04-17 22:20:29 +0200

    enabled adding nodes

--- samples/Gallery/Pages/TreeView/EditableTreeView.fs
+++ samples/Gallery/Pages/TreeView/EditableTreeView.fs
@@ -12,12 +12,15 @@ module EditableNodeView =
     type Node(name, children) =
 
         let mutable _name: string = name
+        let mutable _children: Node list = children
 
         member this.Name
             with get () = _name
             and set value = _name <- value
 
-        member this.Children = children
+        member this.Children
+            with get () = _children
+            and set value = _children <- value
 
     type Model = { Node: Node }
 
@@ -52,7 +55,9 @@ module EditableTreeView =
         { Nodes: EditableNodeView.Node list
           Selected: EditableNodeView.Node option }
 
-    type Msg = SelectionItemChanged of SelectionChangedEventArgs
+    type Msg =
+        | AddNodeTo of string
+        | SelectionItemChanged of SelectionChangedEventArgs
 
     let branch name (children: EditableNodeView.Node list) = EditableNodeView.Node(name, children)
     let leaf name = branch name []
@@ -78,7 +83,7 @@ module EditableTreeView =
 
         { Nodes = nodes; Selected = None }, []
 
-    let rec findNodes (predicate: EditableNodeView.Node -> bool) (nodes: EditableNodeView.Node list) =
+    let rec findNodes (predicate: EditableNodeView.Node -> bool) (nodes: EditableNodeView.Node seq) =
         let rec matches (node: EditableNodeView.Node) =
             if predicate node then
                 seq { node }
@@ -89,6 +94,20 @@ module EditableTreeView =
 
     let update msg model =
         match msg with
+        | AddNodeTo parentNodeName ->
+            let newNode = leaf ""
+
+            let updated =
+                match findNodes (fun n -> n.Name = parentNodeName) model.Nodes |> Seq.tryExactlyOne with
+                | Some node ->
+                    node.Children <- node.Children @ [ newNode ]
+                    model
+                | None ->
+                    { model with
+                        Nodes = model.Nodes @ [ newNode ] }
+
+            updated, Cmd.none
+
         | SelectionItemChanged args ->
             let node = args.AddedItems[0] :?> EditableNodeView.Node
             let modelNode = findNodes (fun n -> n = node) model.Nodes |> Seq.tryExactlyOne
@@ -106,18 +125,43 @@ module EditableTreeView =
 #endif
         )
 
+    /// Functions for leaf nodes for adding a node to a list.
+    module AddLeaf =
+        let private prefix = "addTo!"
+
+        /// Appends an Add leaf Node (referencing the parentNode or the model if None) to the Node list.
+        let appendTo list (parentNode: EditableNodeView.Node option) =
+            let parentName = parentNode |> Option.map(_.Name) |> Option.defaultValue ""
+            list @ [ leaf(prefix + parentName) ]
+
+        /// Identifies an Add leaf Node.
+        let is (node: EditableNodeView.Node) = node.Name.StartsWith(prefix)
+
+        /// Retrieves the parent Node Name from the Node Name of an Add leaf Node.
+        let getParentNodeName (node: EditableNodeView.Node) = node.Name.Substring(prefix.Length)
+
     let view () =
         Component(program) {
             let! model = Mvu.State
 
             HStack() {
                 TreeView(
-                    model.Nodes,
-                    (_.Children),
+                    AddLeaf.appendTo model.Nodes None,
                     (fun node ->
-                        EditableNodeView
-                            .view(node)
-                            .horizontalAlignment(HorizontalAlignment.Left))
+                        if AddLeaf.is node then
+                            null
+                        else
+                            AddLeaf.appendTo node.Children (Some node)),
+                    (fun node ->
+                        HStack(5) {
+                            if AddLeaf.is node then
+                                Button("+", AddNodeTo(AddLeaf.getParentNodeName node))
+                                    .tip(ToolTip("Add a node"))
+                            else
+                                EditableNodeView
+                                    .view(node)
+                                    .horizontalAlignment(HorizontalAlignment.Left)
+                        })
                 )
                     .onSelectionChanged(SelectionItemChanged)
 
