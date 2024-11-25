8e344fbdd45d846ddd500fd541fb2c28bc126048
Edgar Gonzalez <egonzalez@totallymoney.com>
2024-11-25 21:39:37 +0000

    Update Component samples

--- samples/Component/Navigation/ComponentNavigation/PageC.fs
+++ samples/Component/Navigation/ComponentNavigation/PageC.fs
@@ -6,82 +6,81 @@ open Fabulous.Avalonia
 open type Fabulous.Avalonia.View
 
 module PageC =
-    type Model =
-        { IsActive: bool
-          Args: string
-          StepCount: int
-          Count: int }
-
-    type Msg =
-        | Active
-        | Inactive
-        | Increment
-        | Decrement
-        | GoBack
-        | GoToPageA
-        | GoToPageB
-
-    let init (args, stepCount) =
-        { IsActive = false
-          Args = args
-          StepCount = stepCount
-          Count = 0 },
-        Cmd.none
-
-    let update (nav: NavigationController) msg model =
-        match msg with
-        | Active -> { model with IsActive = true }, Cmd.none
-        | Inactive -> { model with IsActive = false }, Cmd.none
-        | Increment ->
-            { model with
-                Count = model.Count + model.StepCount },
-            Cmd.none
-        | Decrement ->
-            { model with
-                Count = model.Count - model.StepCount },
-            Cmd.none
-        | GoBack -> model, Navigation.navigateBack nav
-        | GoToPageA -> model, Navigation.navigateToPageA nav
-        | GoToPageB -> model, Navigation.navigateToPageB nav model.Count
-
-    let subscribe (appMsgDispatcher: IAppMessageDispatcher) model =
-        let localAppMsgSub dispatch =
-            appMsgDispatcher.Dispatched.Subscribe(fun msg ->
-                match msg with
-                | AppMsg.BackButtonPressed -> dispatch GoBack)
-
-        [ if model.IsActive then
-              [ nameof localAppMsgSub ], localAppMsgSub ]
-
-    let program nav appMsgDispatcher =
-        Program.statefulWithCmd init (update nav)
-        |> Program.withSubscription(subscribe appMsgDispatcher)
+    // type Model =
+    //     { IsActive: bool
+    //       Args: string
+    //       StepCount: int
+    //       Count: int }
+    //
+    // type Msg =
+    //     | Active
+    //     | Inactive
+    //     | Increment
+    //     | Decrement
+    //     | GoBack
+    //     | GoToPageA
+    //     | GoToPageB
+    //
+    // let init (args, stepCount) =
+    //     { IsActive = false
+    //       Args = args
+    //       StepCount = stepCount
+    //       Count = 0 },
+    //     Cmd.none
+    //
+    // let update (nav: NavigationController) msg model =
+    //     match msg with
+    //     | Active -> { model with IsActive = true }, Cmd.none
+    //     | Inactive -> { model with IsActive = false }, Cmd.none
+    //     | Increment ->
+    //         { model with
+    //             Count = model.Count + model.StepCount },
+    //         Cmd.none
+    //     | Decrement ->
+    //         { model with
+    //             Count = model.Count - model.StepCount },
+    //         Cmd.none
+    //     | GoBack -> model, Navigation.navigateBack nav
+    //     | GoToPageA -> model, Navigation.navigateToPageA nav
+    //     | GoToPageB -> model, Navigation.navigateToPageB nav model.Count
+    //
+    // let subscribe (appMsgDispatcher: IAppMessageDispatcher) model =
+    //     let localAppMsgSub dispatch =
+    //         appMsgDispatcher.Dispatched.Subscribe(fun msg ->
+    //             match msg with
+    //             | AppMsg.BackButtonPressed -> dispatch GoBack)
+    //
+    //     [ if model.IsActive then
+    //           [ nameof localAppMsgSub ], localAppMsgSub ]
+    //
+    // let program nav appMsgDispatcher =
+    //     Program.statefulWithCmd init (update nav)
+    //     |> Program.withSubscription(subscribe appMsgDispatcher)
 
     let view nav appMsgDispatcher args =
-        Component("PageC") {
-            let! model = Context.Mvu(program nav appMsgDispatcher, args)
+        Component("Page C") {
+            //let! model = Context.Mvu(program nav appMsgDispatcher)
 
-            //ContentPage(
             Grid(coldefs = [ Star ], rowdefs = [ Star; Auto ]) {
                 VStack() {
-                    Label($"Args: {model.Args}")
-                    Label($"StepCount from Page B: {model.StepCount}")
+                    Label("Page C")
+                //Label($"Count: {model.Count}") //.centerTextHorizontal()
 
-                    Label($"Count: {model.Count}") //.centerTextHorizontal()
-
-                    Button("Increment", Increment)
-                    Button("Decrement", Decrement)
+                // Button("Increment", Increment)
+                // Button("Decrement", Decrement)
                 }
 
-                (VStack() {
-                    Button("Go back", GoBack)
-                    Button("Go to Page A", GoToPageA)
-                    Button("Go to Page B", GoToPageB)
-                })
+                (VStack
+                    ()
+                    {
+                    // Button("Go back", GoBack)
+                    // Button("Go to Page B", GoToPageB)
+                    // Button("Go to Page C", GoToPageC)
+                    })
                     .gridRow(1)
             }
-        //)
-        //.title("Page C")
-        //.onNavigatedTo(Active)
-        //.onNavigatedFrom(Inactive)
+
+        // .title("Page A")
+        // .onNavigatedTo(Active)
+        // .onNavigatedFrom(Inactive)
         }
