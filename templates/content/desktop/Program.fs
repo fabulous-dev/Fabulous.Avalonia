ef04b0e201bb5f7785d49df42069ba4788fc055b
edgargonzalez <egonzalez@totallymoney.com>
2025-04-07 14:23:17 +0100

    desktop template

---
+++ templates/content/desktop/Program.fs
@@ -0,0 +1,22 @@
+namespace NewApp
+
+open System
+open Avalonia
+open Fabulous.Avalonia
+
+module Program =
+
+    [<CompiledName "BuildAvaloniaApp">]
+    let buildAvaloniaApp () =
+        AppBuilder
+            .Configure(fun () ->
+                let app = Program.startApplication App.program
+                app.Styles.Add(App.theme)
+                app)
+            .LogToTrace(areas = Array.empty)
+            .UsePlatformDetect()
+
+    [<EntryPoint; STAThread>]
+    let main argv =
+        buildAvaloniaApp().StartWithClassicDesktopLifetime(argv)
+
